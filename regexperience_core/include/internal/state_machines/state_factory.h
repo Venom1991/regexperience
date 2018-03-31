5674c8903081ff5b0ff2483c73b45f9625eafbdf
luka <luka_zaja@yahoo.com>
2018-03-31 17:37:35 +0200

    Style adjustments

--- regexperience_core/include/internal/state_machines/state_factory.h
+++ regexperience_core/include/internal/state_machines/state_factory.h
@@ -4,7 +4,7 @@
 #include "internal/state_machines/state.h"
 #include "internal/state_machines/composite_state.h"
 
-State *create_composite_state (GPtrArray                          *composed_from_states,
+State *create_composite_state (GPtrArray                      *composed_from_states,
                                CompositeStateResolveTypeFlags  resolve_type_flags_mode);
 
 State *create_dead_state      (void);
